33c0f2256728c2bed546820133a716229bf949ac
Felix Siegfried Riedel <f.riedel.business@gmail.com>
2025-04-15 17:58:02 +0200

    add Note class

---
+++ src/main/java/de/htwberlin/blackboard/blackboard/Note.java
@@ -0,0 +1,123 @@
+package de.htwberlin.blackboard.blackboard;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+@Entity(name = "note")
+public class Note {
+    @Id
+    private int id = 0;
+    public  static int idNext;
+
+    private String title;
+    private String content;
+    private String author;
+    private String color;
+
+    private Date creationDate;
+    private Date terminationDate;
+
+    private int xPosition;
+    private int yPosition;
+    private int width;
+    private int height;
+
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public static int getIdNext() {
+        return idNext;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public Date getTerminationDate() {
+        return terminationDate;
+    }
+
+    public int getxPosition() {
+        return xPosition;
+    }
+
+    public int getyPosition() {
+        return yPosition;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setId(int id) {
+        this.id = idNext++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setxPosition(int xPosition) {
+        this.xPosition = xPosition;
+    }
+
+    public static void setIdNext(int idNext) {
+        Note.idNext = idNext;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public void setTerminationDate(Date terminationDate) {
+        this.terminationDate = terminationDate;
+    }
+
+    public void setyPosition(int yPosition) {
+        this.yPosition = yPosition;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+}
