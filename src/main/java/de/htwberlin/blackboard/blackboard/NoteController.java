d6868adef1a9fec0f8d5796ea28e7b1feb38c8b2
Felix Siegfried Riedel <f.riedel.business@gmail.com>
2025-05-07 17:34:02 +0200

    change cors url

--- src/main/java/de/htwberlin/blackboard/blackboard/NoteController.java
+++ src/main/java/de/htwberlin/blackboard/blackboard/NoteController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://blackboard-frontend-inaa.onrender.com")
 @RestController
 public class NoteController {
 
